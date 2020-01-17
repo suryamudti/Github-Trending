f90b62c449bf5f66d92539b43dff17893d44ea88
suryamudti <groundmos@hotmail.com>
2020-01-17 07:58:01 +0700

    update NetworkConnectionInterceptor documentation

--- app/src/main/java/com/surya/githubtrending/data/remote/NetworkConnectionInterceptor.kt
+++ app/src/main/java/com/surya/githubtrending/data/remote/NetworkConnectionInterceptor.kt
@@ -15,14 +15,24 @@ class NetworkConnectionInterceptor(
     context: Context
 ) : Interceptor{
 
+    /**
+     * context attributes will use for the interceptor
+     * */
     private val applicationContext = context.applicationContext
 
+    /**
+     * this will override intercept function from [Interceptor] class
+     * */
     override fun intercept(chain: Interceptor.Chain): Response {
         if (!isInternetAvailable())
             throw NoInternetException("Make sure you have an active data connection")
         return chain.proceed(chain.request())
     }
 
+    /**
+     * this function will check the availability of the internet
+     * @return true if internet available, no for otherwise
+     * */
     private fun isInternetAvailable():Boolean{
         var result = false
         val connectivityManager =
