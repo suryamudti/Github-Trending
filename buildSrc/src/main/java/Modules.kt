8b9351fb95243c4de48586a57cd31d83ac603549
suryamudti <groundmos@hotmail.com>
2020-01-18 22:14:38 +0700

    update Modules

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,15 @@
+
+object Modules {
+    //parent app
+    val app = ":app"
+
+    //feature
+    val trends = ":trends"
+
+    //libraries
+    val network = ":libraries:network"
+    val abstraction = ":libraries:abstraction"
+
+    //data
+    val data = ":data"
+}
